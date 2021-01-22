d83eb6df8c93a60d8bcdeda07546d9d65186cb43
Shubham Gupta <311297sg@gmail.com>
2021-01-22 20:02:24 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
+import { ParentComponent } from './parent/parent.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent,
+    ParentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     RouterModule.forRoot([
+      { path:'',component:ParentComponent , pathMatch: 'full' 
+
+      }
+     ])
   ],
   providers: [],
   bootstrap: [AppComponent]
